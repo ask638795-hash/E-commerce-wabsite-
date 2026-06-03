611a326b76e4d3795d470ab947810b6a02c4be95
ask638795-hash <ask638795@gmail.com>
2026-06-03 09:11:56 +0600

    Add CheckoutPage component with basic layout

---
+++ src/pages/CheckoutPage.tsx
@@ -0,0 +1,9 @@
+export default function CheckoutPage() {
+  return (
+    <div className="container py-10">
+      <h1 className="text-4xl font-bold">
+        Checkout
+      </h1>
+    </div>
+  );
+}
