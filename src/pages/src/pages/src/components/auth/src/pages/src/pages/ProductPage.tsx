d1e2bd910abbd54552c60d88b177b007d9449f86
ask638795-hash <ask638795@gmail.com>
2026-06-03 09:10:02 +0600

    Create ProductPage component for product details

---
+++ src/pages/src/pages/src/components/auth/src/pages/src/pages/ProductPage.tsx
@@ -0,0 +1,9 @@
+export default function ProductPage() {
+  return (
+    <div className="container py-10">
+      <h1 className="text-4xl font-bold">
+        Product Details
+      </h1>
+    </div>
+  );
+}
